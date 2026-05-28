868f08c6b5fe1d24f8bdcc593399389875d80b49
meuprogramadorr <kg884010@gmail.com>
2026-05-28 14:43:02 +0000

    fix

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,7 +10,7 @@ float AREA= 1153.11;
 float PIB= 44206.07;
 int TURISMOS= 5;
 
-printf("--CARTA 1--\n");
+printf("--CARTA 01--\n");
 
 printf("ESTADO:");
 scanf("%s", ESTADO);
@@ -33,14 +33,74 @@ scanf("%f", &PIB);
 printf("NUMERO DE PONTOS TURISTICOS:");
 scanf("%d", &TURISMOS);
 
-printf("\n --- CARTA 1 ---\n");
+printf("\n --- CARTA 01 ---\n");
 printf("ESTADO:%s\n", ESTADO);
 printf("CODIGO:%s\n", CODIGO);
 printf("CIDADE:%s\n", CIDADE);
 printf("POPULACAO:%d\n", POPULACAO);
-printf("AREA:%.2fKM²\n", AREA);
-printf("PIB:%.2f\n", PIB);
-printf("TURISMOS:%d\n", TURISMOS);
+printf("AREA:%.2fkm²\n", AREA);
+printf("PIB:%.2fREAIS\n", PIB);
+printf("TURISMOS:%dPONTOS TURISTICOS\n", TURISMOS);
+
+
+
+     
+    char ESTADO1[10]="MG";
+    char CODIGO1[15]="M02";
+    char CIDADE1[20]="BeloHorizonte";
+    int POPULACAO1=2434975;
+    float AREA1=331.35;
+    float PIB1=130.2;
+    int TURISMOS1=17;
+     
+    printf("\n");
+
+    printf("---CARTA 02---\n");
+
+    printf("ESTADO1:");
+    scanf(" %s:", ESTADO1);
+
+    printf("CODIGO1:");
+    scanf(" %s:", CODIGO1);
+
+    printf("CIDADE1:");
+    scanf(" %s:", CIDADE1);
+
+    printf("POPULACAO1:");
+    scanf(" %d:", &POPULACAO1);
+
+    printf("AREA EM KM²1:");
+    scanf(" %f:", &AREA1);
+
+    printf("PIB EM MILHOES1:");
+    scanf(" %.2f:", &PIB1);
+
+    printf("NUMERO DE PONTOS TURISCOS1:");
+    scanf(" %d:", &TURISMOS1);
+
+    printf("\n---CARTA 02---\n");
+    printf("ESTADO: %s\n", ESTADO1);
+    printf("CODIGO: %s\n", CODIGO1);
+    printf("CIDADE: %s\n", CIDADE1);
+    printf("POPULACAO: %d\n", POPULACAO1);
+    printf("AREA EM KM²1: %.2fKm²\n", AREA1);
+    printf("PIB EM MILHOES1: %.2fREAIS\n", PIB1 );
+    printf("TURISMOS1: %dPONTOS TURISTICOS\n", TURISMOS1);
+
+
+    return 0;
+
+
+    
+
+
+
+
+
+
+}
+
+    
 
 
 
@@ -48,10 +108,39 @@ printf("TURISMOS:%d\n", TURISMOS);
 
 
 
-return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   
 
 
-} 
+
